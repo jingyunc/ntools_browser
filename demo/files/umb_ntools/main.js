86d8e691d7e0fc21fd41fcf825a6fee1e08a0d7a
jpb93 <jeremy.burkhardt@gmail.com>
2021-11-08 22:33:07 -0500

    Destructure arrays and make variable names descriptive

--- demo/files/umb_ntools/main.js
+++ demo/files/umb_ntools/main.js
@@ -6,22 +6,21 @@ function load_volume() {
 };
 
 function load_surfaces() {
-    var lh = new X.mesh();
+    var leftHemisphere = new X.mesh();
     // var rh = new X.mesh();
     // var p = new X.mesh();
 
-    lh.file = '../fsaverage/lh.pial';
-    lh.color = [1, 1, 1];
-    lh.opacity = 0.3;
-    
-    
-    return [lh];
+    leftHemisphere.file = '../fsaverage/lh.pial';
+    leftHemisphere.color = [1, 1, 1];
+    leftHemisphere.opacity = 0.3;
+
+    return [leftHemisphere];
 };
 
 function setup_renderers() {
-    var r = new X.renderer3D();
-    r.container = '3d';
-    r.init();
+    var threeDRenderer = new X.renderer3D();
+    threeDRenderer.container = '3d';
+    threeDRenderer.init();
 
     var sliceX = new X.renderer2D();
     sliceX.container = 'sliceX';
@@ -38,23 +37,19 @@ function setup_renderers() {
     sliceZ.orientation = 'Z';
     sliceZ.init();
 
-    return [r, sliceX, sliceY, sliceZ];
+    return [threeDRenderer, sliceX, sliceY, sliceZ];
 }
 
 window.onload = function() {
     
-    all_renderers = setup_renderers();
-
-    threeD = all_renderers[0];
-    sliceX = all_renderers[1];
-    sliceY = all_renderers[2];
-    sliceZ = all_renderers[3];
+    // destructure array
+    var [threeD, sliceX, sliceY, sliceZ] = setup_renderers();
 
     volume = load_volume();
 
     load_electrodes(threeD);
 
-    surfaces = load_surfaces();
+    var [leftHemisphereMesh] = load_surfaces();
     
     sliceX.add(volume);
     sliceX.render();
@@ -68,11 +63,11 @@ window.onload = function() {
         sliceY.render();
         sliceZ.add(volume);
         sliceZ.render();
-        
 
         threeD.add(volume);
-        threeD.add(surfaces[0]);
+        threeD.add(leftHemisphereMesh);
 
+        // fix original camera position
         threeD.camera.position = [0, 360, 0];
         threeD.camera.rotate(new X.vector(450, 0, 0))
 
@@ -81,7 +76,7 @@ window.onload = function() {
 
     // the onShowtime function gets called automatically, just before the first rendering happens
     threeD.onShowtime = function() {
-
+       
 
         
     
